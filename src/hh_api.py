211f2330086bb8fbb93d730134780c7599017ab6
Dmitry <dima.captan@yandex.ru>
2023-07-14 15:30:00 +0300

    added class HHAPI

---
+++ src/hh_api.py
@@ -0,0 +1,51 @@
+import requests
+
+from src.employer import Employer
+from src.vacancy import Vacancy
+
+
+class HHAPI:
+    '''
+    Класс для работы с API сайта
+    https://hh.ru/
+    '''
+
+    def __init__(self, employers_id_list: list) -> None:
+        self.__api = 'https://api.hh.ru/'
+        self.employers_list = employers_id_list
+
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}({self.employers_list})'
+
+    def __str__(self) -> str:
+        return 'API сайта https://hh.ru/'
+
+    def get_employers(self) -> list:
+        '''
+        Получает список работадателей, созданных
+        на основе класса Employer
+        :return: список объектов класса Employer
+        '''
+        employer_list = []
+        for employer_id in self.employers_list:
+            response = requests.get(url=f'{self.__api}employers/{employer_id}').json()
+            employer = Employer(
+                response['name'],
+                response['type'],
+                response['area']['name'],
+                response['description'],
+                response['alternate_url'],
+                response['site_url'],
+                response['vacancies_url'],
+                response['open_vacancies']
+            )
+
+            employer_list.append(employer)
+
+        return employer_list
+
+
+b = ['4370786']
+a = HHAPI(b)
+c = a.get_employers()
+
