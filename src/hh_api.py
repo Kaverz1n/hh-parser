116535342c15e59d06e17579a8350f806e46c440
Dmitry <dima.captan@yandex.ru>
2023-07-17 17:01:46 +0300

    changed name of variable which has list that consists of employers id and added exception

--- src/hh_api.py
+++ src/hh_api.py
@@ -11,10 +11,10 @@ class HHAPI:
 
     def __init__(self, employers_id_list: list) -> None:
         self.__api = 'https://api.hh.ru/'
-        self.employers_list = employers_id_list
+        self.employers_id_list = employers_id_list
 
     def __repr__(self) -> str:
-        return f'{self.__class__.__name__}({self.employers_list})'
+        return f'{self.__class__.__name__}({self.employers_id_list})'
 
     def __str__(self) -> str:
         return 'API сайта https://hh.ru/'
@@ -26,19 +26,22 @@ class HHAPI:
         :return: список объектов класса Employer
         '''
         employer_list = []
-        for employer_id in self.employers_list:
+        for employer_id in self.employers_id_list:
             response = requests.get(url=f'{self.__api}employers/{employer_id}').json()
 
-            employer = Employer(
-                response['name'],
-                response['type'],
-                response['area']['name'],
-                response['description'],
-                response['alternate_url'],
-                response['site_url'],
-                response['vacancies_url'],
-                response['open_vacancies']
-            )
+            try:
+                employer = Employer(
+                    response['name'],
+                    response['type'],
+                    response['area']['name'],
+                    response['description'],
+                    response['alternate_url'],
+                    response['site_url'],
+                    response['vacancies_url'],
+                    response['open_vacancies']
+                )
+            except KeyError:
+                raise KeyError(f'Работадателя с id {employer_id} не существует')
 
             employer_list.append(employer)
 
