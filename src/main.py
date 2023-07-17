71a24f56ca8db82493dc3c859f09859883b5917e
Dmitry <dima.captan@yandex.ru>
2023-07-17 17:02:12 +0300

    added funcs from utils

--- src/main.py
+++ src/main.py
@@ -1,5 +1,12 @@
+from utils import *
+
+
 def main():
-    pass
+    employers_id_list = ['9340246', '2180']
+    database = choose_bd(employers_id_list)
+
+    menu(database)
+
 
 if __name__ == '__main__':
     main()
