9cf40108b30656bdcfd67bf0af15fba3ea66765c
Dmitry <dima.captan@yandex.ru>
2023-07-22 14:07:05 +0300

    added employers id in employers_id_list

--- src/main.py
+++ src/main.py
@@ -2,7 +2,10 @@ from utils import *
 
 
 def main():
-    employers_id_list = ['9340246', '2180']
+    employers_id_list = [
+        '2180', '1455', '87021', '3529', '1942330', '2624085',
+        '3093544', '1122462', '3112647', '10079206'
+    ]
     database = choose_bd(employers_id_list)
 
     menu(database)
